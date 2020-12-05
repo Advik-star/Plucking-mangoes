cb6ba5e10670276ab46908b0320d89aa73b20f2b
Advik-star <71903276+Advik-star@users.noreply.github.com>
2020-12-05 14:53:12 -0500

    Add files via upload

---
+++ boy.js
@@ -0,0 +1,30 @@
+// class boy
+// {
+// 	constructor(x,y,w,h)
+// 	{
+// 		var options={
+// 			isStatic:true			
+// 			}
+// 		this.x=x;
+// 		this.y=y;
+// 		this.w=w
+//         this.h=h
+//         this.image=loadImage("boy.png")
+// 		this.body=Bodies.rectangle(x, y, w, h , options);
+//  		World.add(world, this.body);
+// 	}
+	
+// 	display()
+// 	{		
+// 		var boyPos=this.body.position;		
+
+// 		push()
+// 		translate(boyPos.x, boyPos.y);
+// 		rectMode(CENTER)
+// 		//strokeWeight(4);
+// 		fill(128,128,128)
+// 		imageMode(CENTER);
+// 		image(this.image,0,0,this.w, this.h);
+// 		pop()			
+// 	}
+// }
